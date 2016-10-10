6d2e6159422556ffa673d4c2cdf1ef336316d04d
Anthony Gaona <gaonaanthony@gmail.com>
2016-10-09 22:51:49 -0700

    installed gulp concat and gulp uglify. updated gulpfile with tasks for these dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-coffee'),
+    uglify = require('gulp-uglify');
 
 
 var coffeeSources = ['components/coffee/confirm.coffee']; //code each individual file or use *.coffee
+var jsSources = [
+    'components/scripts/confirm.js',
+    'components/scripts/depOne.js',
+    'components/scripts/depTwo.js'
+];
 
 gulp.task('coffee', function() {
     gulp.src(
@@ -14,3 +21,12 @@ gulp.task('coffee', function() {
         .pipe(gulp.dest('components/scripts'));    
 });
 
+gulp.task('concat', function() {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('uglify', function() {
+    console.log('minified')
+});
